a83f840272d83da6ef8bdb8a9c4b68ce8dd030e1
altumus <jojojopa@icloud.com>
2022-08-21 11:38:41 +0300

    изменение названия задачи

--- src/components/ProductsScreen.vue
+++ src/components/ProductsScreen.vue
@@ -6,7 +6,11 @@
     >
       <div class="w-[350px] bg-[#F9F7F7] rounded border-[#00ADB5] border-[1px] h-[60px] mb-[20px] mr-[10px]">
         <div class="flex flex-col p-[5px]">
-          <span class="font-bold text-[16px]">{{ product.name }}</span>
+          <input
+            class="font-bold text-[16px] bg-[#F9F7F7] focus:outline-none"
+            :value="product.name"
+            @blur="showMessage"
+          >
           <span class="text-[15px] text-[#7E7E80]">Кол-во товара: {{ product.amount }}</span>
         </div>
       </div>
@@ -18,6 +22,11 @@ export default {
   computed: {
     products () {
       return this.$store.state.products.products
+    }
+  },
+  methods: {
+    showMessage (e) {
+      alert(`Теперь этот продукт называется - ${e.target.value} !`)
     }
   }
 }
